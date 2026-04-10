f825af41a92bcbeb911c72f4f73e115019a2d571
qirong77 <qirong7777@gmail.com>
2026-04-10 11:46:06 +0800

    feat: 功能开发 ✨

--- 155.254.127.215/index.mjs
+++ 155.254.127.215/index.mjs
@@ -1,8 +1,5 @@
 import http from "http";
-
-/** 与 nginx-config.sh 中 INTERNAL_PORT 一致；由 nginx 对外提供 443 / IP:3000 的 HTTPS */
 const port = 3000;
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
     res.end("Port test success! Server is running.");
