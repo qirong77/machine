8ab13632ba5a473881f23a6d3034b9bf1d2d8b30
qirong77 <qirong7777@gmail.com>
2026-04-10 13:18:08 +0800

    feat: 功能开发 ✨

--- 155.254.127.215/ping.mjs
+++ 155.254.127.215/ping.mjs
@@ -1,5 +1,6 @@
-fetch('api.qirong77.com').then(res => res.json()).then(data => {
+// 在客户端ping API
+fetch('https://api.qirong77.com').then(res => res.text()).then(data => {
     console.log(data);
 }).catch(err => {
-    console.error(err);
+    console.error(err.message);
 });
